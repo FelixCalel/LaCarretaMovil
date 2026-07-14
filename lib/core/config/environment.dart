abe795b0df0ec50ec37a6eaa93e69ad428324d45
Félix Calel <desarrollo1@popoyan.com.gt>
2026-07-14 15:43:48 -0600

    chore: remove staging URL from environment for security

--- lib/core/config/environment.dart
+++ lib/core/config/environment.dart
@@ -12,16 +12,16 @@ class Environment {
 
     // Si estamos en modo release (APK compilado o Shorebird), usar el servidor real
     if (kReleaseMode) {
-      return 'https://stg-caback.popoyan.com.gt/api';
+      return 'http://localhost:3000/api';
     }
 
     // Valores de desarrollo por defecto (modo debug)
     if (kIsWeb) {
-      return 'https://stg-caback.popoyan.com.gt/api';
+      return 'http://localhost:3000/api';
     } else if (Platform.isAndroid) {
-      return 'https://stg-caback.popoyan.com.gt/api';
+      return 'http://10.0.2.2:3000/api';
     } else {
-      return 'https://stg-caback.popoyan.com.gt/api';
+      return 'http://localhost:3000/api';
     }
   }
 }
